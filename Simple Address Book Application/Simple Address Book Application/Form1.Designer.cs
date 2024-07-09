d96a8d83c238130c9db8d9af6dc63d2fcfb4fdaa
opganiyu <103090859+opganiyu@users.noreply.github.com>
2024-07-09 23:40:14 +0000

    Add files via upload

---
+++ Simple Address Book Application/Simple Address Book Application/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AddressBookApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private TextBox nameTextBox;
+        private TextBox emailTextBox;
+        private TextBox phoneTextBox;
+        private Label nameLabel;
+        private Label emailLabel;
+        private Label phoneLabel;
+        private Button saveButton;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new TextBox();
+            this.emailTextBox = new TextBox();
+            this.phoneTextBox = new TextBox();
+            this.nameLabel = new Label();
+            this.emailLabel = new Label();
+            this.phoneLabel = new Label();
+            this.saveButton = new Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(120, 50);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(150, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(120, 100);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(150, 20);
+            this.emailTextBox.TabIndex = 1;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(120, 150);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(150, 20);
+            this.phoneTextBox.TabIndex = 2;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(50, 53);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(50, 103);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(32, 13);
+            this.emailLabel.TabIndex = 4;
+            this.emailLabel.Text = "Email";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(50, 153);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(38, 13);
+            this.phoneLabel.TabIndex = 5;
+            this.phoneLabel.Text = "Phone";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(120, 200);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 270);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "Form1";
+            this.Text = "Address Book";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
